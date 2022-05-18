08370f137071f1c11549f3db3e1eafa67ba62114
Cuongct23 <cuongquanbai2@gmail.com>
2022-05-18 21:39:56 +0700

    Đẩy master

---
+++ app/src/main/java/com/example/quanlyagile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.quanlyagile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_bill_entry_chitiet);
+    }
+}
